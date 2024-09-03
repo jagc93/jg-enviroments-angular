e0123b7a34fc3cd2ded0bfa8d43d175b5b149d17
jagc93 <jagc93.jg@gmail.com>
2024-09-03 16:51:26 -0500

    Se usa la enviroment message en el app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { enviroment } from '../enviroments/enviroment';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'enviroments';
+
+  constructor() {
+    console.log(`Enviroment message: ${enviroment.message}`);
+  }
 }
